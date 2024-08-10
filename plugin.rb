69025ad9309523fd3d632557a0993585d91735fa
Tyler Kinney <tylerkinney50@gmail.com>
2024-08-10 18:18:22 -0500

    Initial commit

---
+++ plugin.rb
@@ -0,0 +1,40 @@
+# name: discourse-swot-validation
+# about: Validates email sign-ups against the swot gem to check for academic email addresses
+# version: 0.1
+# authors: Your Name
+# url: https://github.com/tpglitch/discourse-swot-validator
+
+enabled_site_setting :swot_validation_enabled
+
+gem 'swot', github: 'jetbrains/swot'
+
+after_initialize do
+  module ::SwotValidation
+    class Engine < ::Rails::Engine
+      engine_name 'swot_validation'
+      isolate_namespace SwotValidation
+    end
+  end
+
+  # Extend the existing UserValidator with swot validation
+  require_dependency 'user_validator'
+
+  class ::UserValidator
+    def validate_email(email)
+      super(email)
+      if SiteSetting.swot_validation_enabled && Swot::is_academic?(email)
+        # If the email is academic, we continue with the sign-up process
+        return
+      elsif SiteSetting.swot_validation_enabled
+        # If the email is not academic and validation is enabled, add an error
+        @errors.add(:email, I18n.t('user.email_is_not_academic'))
+      end
+    end
+  end
+end
+
+# Add translations
+register_locale_file "config/locales/server.en.yml", :en
+
+# Configuration Settings
+enabled_site_setting :swot_validation_enabled
